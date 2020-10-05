63186a3fc567fe5521657b016ebfbf7e3ce4b59e
Sami <sami79031@yahoo.com>
2020-10-05 14:34:50 +0300

    LocalFeedStore moved to a folder

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeedTests
+//
+//  Created by Sami Ali on 10/5/20.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
