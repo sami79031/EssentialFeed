b9f7eb0bd7065f12aa9e44fba1eafada49da0e64
Sami <sami79031@yahoo.com>
2020-10-06 14:34:15 +0300

    Load command requests cache retrieval

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -9,9 +9,11 @@ import Foundation
 import EssentialFeed
 
 class FeedStoreSpy: FeedStore {
+    
     enum ReceivedMessage: Equatable {
         case deleteCachedFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
     }
     
     private(set) var receivedMessages = [ReceivedMessage]()
@@ -45,4 +47,7 @@ class FeedStoreSpy: FeedStore {
         insertionCompletions[index](nil)
     }
 
+    func retrieve() {
+        receivedMessages.append(.retrieve)
+    }
 }
