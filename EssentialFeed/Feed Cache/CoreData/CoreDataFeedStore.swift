8805d27e93428d06ed3db9d851d0ddd4cdbe5998
Sami <sami79031@yahoo.com>
2020-10-09 14:39:33 +0300

    `CoreDataFeedStore.retrieve()` delivers empty on empty cache

---
+++ EssentialFeed/Feed Cache/CoreData/CoreDataFeedStore.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Sami Ali on 10/9/20.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    public init() {}
+
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+
+    }
+
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+
+    }
+
+}
---
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreHelpers.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestCase+FeedStoreHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Sami Ali on 10/9/20.
+//
+
+import XCTest
+import EssentialFeed
+
+extension FeedStoreSpecs where Self: XCTestCase {
+    
+    func assertThatRetrieveDeliversEmptyOnEmptyCache(on sut: FeedStore, file: StaticString = #filePath, line: UInt = #line) {
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+    
+}
